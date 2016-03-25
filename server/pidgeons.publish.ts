de3f36d115be0f21d8d548729533f06078eb89e9
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2016-03-25 16:29:10 +0100

    Pass the options object to the publish method.

--- server/pidgeons.publish.ts
+++ server/pidgeons.publish.ts
@@ -1,13 +1,13 @@
 import {PidgeonCollection} from '../collections/pidgeons';
 
-Meteor.publish('pidgeons', function() {
+Meteor.publish('pidgeons', function(options: Object) {
     return PidgeonCollection.find({
-        $and: [ { owner: this.userId }, { owner: { $exists: true } } ]
-    });
+        $and: [{ owner: this.userId }, { owner: { $exists: true } }]
+    }, options);
 });
 
 Meteor.publish('pidgeon', function publishPidgeon(pidgeonId: string) {
     return PidgeonCollection.find({
-        $and: [ { _id: pidgeonId }, { owner: this.userId }, { owner: { $exists: true } } ]
+        $and: [{ _id: pidgeonId }, { owner: this.userId }, { owner: { $exists: true } }]
     });
 });
