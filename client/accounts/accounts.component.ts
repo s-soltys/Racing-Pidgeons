eae74070fb6a15ff87fbc9b92b32270be8f08702
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2016-03-29 16:21:55 +0200

    Fix accounts ui component bugs.

--- client/accounts/accounts.component.ts
+++ client/accounts/accounts.component.ts
@@ -1,6 +1,7 @@
 import {Component, NgZone, provide} from 'angular2/core';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Router} from 'angular2/router';
+import {MeteorComponent} from 'angular2-meteor';
 
 // Components
 import {DisplayUser} from '../lib/display-user.pipe';
@@ -10,20 +11,21 @@ import {DisplayUser} from '../lib/display-user.pipe';
     templateUrl: 'client/accounts/accounts.template.html',
     pipes: [DisplayUser]
 })
-export class AccountsUI {
+export class AccountsUI extends MeteorComponent {
     user: Meteor.User;
 
     constructor(private router: Router, private zone: NgZone) {
-        this.user = Meteor.user();
+        super();
+
+        this.autorun(() => {
+            this.user = Meteor.user();
+        });
     }
 
     login(email: string, password: string) {
         Meteor.loginWithPassword(email, password, (err) => {
-            this.zone.run(() => {
-                this.user = Meteor.user();
-                if (err) alert(err);
-                else this.router.navigate(['/Dashboard']);
-            });
+            if (err) alert(err);
+            else this.router.navigate(['/Dashboard']);
         });
     }
 
@@ -31,21 +33,15 @@ export class AccountsUI {
         let credentials = { email: email, password: password };
 
         Accounts.createUser(credentials, (err) => {
-            this.zone.run(() => {
-                this.user = Meteor.user();
-                if (err) alert(err);
-                else this.router.navigate(['/Dashboard']);
-            });
+            if (err) alert(err);
+            else this.router.navigate(['/Dashboard']);
         });
     }
 
     logout() {
         Meteor.logout(err => {
-            this.zone.run(() => {
-                this.user = Meteor.user();
-                if (err) alert(err);
-                else this.router.navigate(['/LandingPage']);
-            });
+            if (err) alert(err);
+            else this.router.navigate(['/LandingPage']);
         });
     }
 }
